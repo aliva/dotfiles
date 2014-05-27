d49c0ff5b81244ba8b19e10c1ea13065f92dc81d
Ali Vakilzade <ali@vakilzade.com>
2014-05-27 22:33:19 +0430

    generate cleanup

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -8,11 +8,6 @@ function die {
     exit 1
 }
 
-function get {
-    echo $2
-    wget -q $1 -O $ROOT/$2 || die $1 $2
-}
-
 # regenerate gitignore file
 echo gitignore
 tail -n +3 $ROOT/hgignore > $ROOT/gitignore
@@ -27,28 +22,3 @@ sudo crontab -l > $ROOT/crontab/root
 echo crontab/`whoami`
 crontab -l > $ROOT/crontab/`whoami`
 
-# vudnle
-if ! [ -d $ROOT/vim/bundle/vundle ]
-then
-    git clone https://github.com/gmarik/vundle.git $ROOT/vim/bundle/vundle
-fi
-
-# do not download these files every time, they don't change often
-exit
-
-# hg bash-autocompltion
-get http://selenic.com/hg/raw-file/tip/contrib/bash_completion bash-completion/mercurial.bash
-
-# pip bash-autocompltion
-get https://raw.githubusercontent.com/ekalinin/pip-bash-completion/master/pip bash-completion/pip.bash
-
-# hg vim plugin
-get http://selenic.com/hg/raw-file/tip/contrib/vim tmp
-sed -i '/^$/d' $ROOT/tmp
-for f in `cat $ROOT/tmp | awk '{print $3}'`
-do
-    get http://selenic.com/hg/raw-file/tip/contrib/vim/$f vim/plugin/$f
-done
-rm $ROOT/tmp
-
-echo "* DONE! *"

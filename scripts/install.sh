b0a3a7b21e6213e4616f875b7b8812dfafb8e51c
Ali Vakilzade <ali@vakilzade.com>
2014-05-27 22:34:06 +0430

    generate section removed

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,28 +13,11 @@ function get {
     wget -q $1 -O $ROOT/$2 || die $1 $2
 }
 
-# regenerate gitignore file
-echo gitignore
-tail -n +3 $ROOT/hgignore > $ROOT/gitignore
-
-# regenerate bzri ignore file
-echo bazaar/ignore
-tail -n +3 $ROOT/hgignore > $ROOT/bazaar/ignore
-
-# crontab
-echo crontab/root
-sudo crontab -l > $ROOT/crontab/root
-echo crontab/`whoami`
-crontab -l > $ROOT/crontab/`whoami`
-
 # vudnle
 if ! [ -d $ROOT/vim/bundle/vundle ]
 then
     git clone https://github.com/gmarik/vundle.git $ROOT/vim/bundle/vundle
 fi
-
-# do not download these files every time, they don't change often
-exit
 
 # hg bash-autocompltion
 get http://selenic.com/hg/raw-file/tip/contrib/bash_completion bash-completion/mercurial.bash

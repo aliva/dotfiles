b04dedb7d8910b23d3d89e34699fc3fe78b64507
Ali Vakilzade <ali@vakilzade.com>
2014-12-11 12:56:04 +0330

    tmux added

--- scripts/xubuntu-14.10-init.sh
+++ scripts/xubuntu-14.10-init.sh
@@ -20,7 +20,7 @@ apt-get install -y apt-fast
 apt-get -y purge gimp gmusicbrowser parole abiword gnumeric
 apt-fast -y upgrade
 apt-fast -y dist-upgrade
-apt-fast -y install clementine tor git mercurial gitg python-pip python-gpgme guake nautilus nautilus-open-terminal gedit gnome-terminal bumblebee tlp tlp-rdw ubuntu-restricted-extras libavcodec-extra vlc openjdk-8-jdk vim-gnome wine mongodb
+apt-fast -y install clementine tor git mercurial gitg python-pip python-gpgme guake nautilus nautilus-open-terminal gedit gnome-terminal bumblebee tlp tlp-rdw ubuntu-restricted-extras libavcodec-extra vlc openjdk-8-jdk vim-gnome wine mongodb tmux
 apt-get -y autoremove
 
 pip install shadowsocks

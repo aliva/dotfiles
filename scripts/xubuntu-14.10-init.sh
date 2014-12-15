418acc63bdc66138b2c8908ed3088fffe72ac580
Ali Vakilzade <ali@vakilzade.com>
2014-12-15 15:54:40 +0330

    aboword seems usefull (it's smaller then libreoffice and can be used as a doc viewer)

--- scripts/xubuntu-14.10-init.sh
+++ scripts/xubuntu-14.10-init.sh
@@ -17,7 +17,7 @@ add-apt-repository -y ppa:linrunner/tlp
 
 apt-get update
 apt-get install -y apt-fast
-apt-get -y purge gimp gmusicbrowser parole abiword gnumeric
+apt-get -y purge gimp gmusicbrowser parole gnumeric
 apt-fast -y upgrade
 apt-fast -y dist-upgrade
 apt-fast -y install clementine tor git mercurial gitg python-pip python-gpgme guake nautilus nautilus-open-terminal gedit gnome-terminal bumblebee tlp tlp-rdw ubuntu-restricted-extras libavcodec-extra vlc openjdk-8-jdk vim-gnome wine mongodb tmux axel curl synaptic

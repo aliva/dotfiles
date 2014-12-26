666390400e7af89bd0ca7a569bd3398aa4107f42
Ali Vakilzade <ali@vakilzade.com>
2014-12-26 18:18:28 +0330

    keep gnumeric

--- scripts/xubuntu-14.10-init.sh
+++ scripts/xubuntu-14.10-init.sh
@@ -17,7 +17,7 @@ add-apt-repository -y ppa:linrunner/tlp
 
 apt-get update
 apt-get install -y apt-fast
-apt-get -y purge gimp gmusicbrowser parole gnumeric
+apt-get -y purge gimp gmusicbrowser parole
 apt-fast -y upgrade
 apt-fast -y dist-upgrade
 apt-fast -y install clementine tor git git-extras mercurial gitg python-pip python-gpgme guake nautilus nautilus-open-terminal gedit gnome-terminal gnome-system-monitor bumblebee tlp tlp-rdw ubuntu-restricted-extras libavcodec-extra vlc openjdk-8-jdk vim-gnome wine mongodb tmux axel curl synaptic autojump

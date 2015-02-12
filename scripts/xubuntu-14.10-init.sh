c888fa49439017578c996efb45a2647da5d846b3
Ali Vakilzade <ali@vakilzade.com>
2015-02-13 00:25:02 +0330

    installer script updated

--- scripts/xubuntu-14.10-init.sh
+++ scripts/xubuntu-14.10-init.sh
@@ -17,22 +17,15 @@ add-apt-repository -y ppa:linrunner/tlp
 
 apt-get update
 apt-get install -y apt-fast
-apt-get -y purge gimp gmusicbrowser parole
+apt-get -y purge gimp gmusicbrowser parole apport*
 apt-fast -y upgrade
 apt-fast -y dist-upgrade
-apt-fast -y install clementine tor git git-extras mercurial gitg python-pip python-gpgme guake nautilus nautilus-open-terminal gedit gnome-terminal gnome-system-monitor bumblebee tlp tlp-rdw ubuntu-restricted-extras libavcodec-extra vlc openjdk-8-jdk vim-gnome wine mongodb tmux axel curl synaptic autojump
+apt-fast -y install clementine tor git mercurial gitg python-pip python3-pip python-gpgme guake gedit gnome-terminal gnome-system-monitor bumblebee tlp tlp-rdw ubuntu-restricted-extras libavcodec-extra vlc openjdk-8-jdk vim-gnome wine tmux axel curl synaptic autojump
 apt-get -y autoremove
 
+# sslocal
 pip install shadowsocks
-
-# mongo
-service mongodb stop
-update-rc.d -f mongodb remove
 
 # tor
 cp ../torrc /etc/tor/torrc
 service tor restart
-
-# bug report
-service apport stop
-update-rc.d -f apport remove

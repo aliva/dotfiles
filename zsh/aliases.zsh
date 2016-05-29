186d390ed5356d6c4993b69950153709dba83a11
Ali Vakilzade <ali@vakilzade.com>
2016-05-29 09:12:37 +0430

    pc for proxychains

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -17,6 +17,7 @@ alias p8="ping 8.8.8.8 -i 3"
 alias axel="axel -a -n 50"
 alias wget="wget -c"
 alias aria2c="aria2c --dir=$HOME/Downloads/aria2c"
+alias pc="proxychains"
 # path
 alias cdd="cd ~/Workspace/dotfiles"
 alias cdw="cd ~/Workspace"

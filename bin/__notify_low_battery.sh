4907c01af800d02d0eafb74ae7d7ea25d26dfa8b
Ali Vakilzade <ali@vakilzade.com>
2017-04-10 17:26:41 +0430

    check if charging

--- bin/__notify_low_battery.sh
+++ bin/__notify_low_battery.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 export DISPLAY=:0
+
+if upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep --quiet "state:\s*charging"
+then
+    exit 0
+fi
+
 export power=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | \
     grep percentage | cut -d: -f 2 | xargs | cut -d% -f1 | xargs`
 

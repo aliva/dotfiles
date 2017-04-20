79ddccef7e82f565b0d203e90c0b8a560258362b
Ali Vakilzade <ali@vakilzade.com>
2017-04-20 11:26:24 +0430

    wow!

--- bin/__notify_low_battery.sh
+++ bin/__notify_low_battery.sh
@@ -10,7 +10,7 @@ fi
 export power=`upower -i /org/freedesktop/UPower/devices/battery_BAT0 | \
     grep percentage | cut -d: -f 2 | xargs | cut -d% -f1 | xargs`
 
-if [[ power -lt 20 ]]
+if [[ power -lt 25 ]]
 then
     notify-send --icon=battery-low "$power% Battery" --expire-time=1
 fi

ca79e93e2a05055a9cadb8460f55fb077e7b4bf9
Ali Vakilzade <ali@vakilzade.com>
2019-01-13 10:11:45 +0330

    Update fish/functions/fish_right_prompt.fish

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -1,15 +1,18 @@
 
 function fish_right_prompt
     set -g fish_prompt_pwd_dir_length 20
-    set -g __fish_git_prompt_showdirtystate 1
-    set -g __fish_git_prompt_showstashstate 1
-    set -g __fish_git_prompt_showuntrackedfiles 1
-    set -g __fish_git_prompt_showupstream verbose
+
     set_color 8be9fd
     echo (prompt_pwd)
 
-    set_color 6272a4
-    echo (__fish_git_prompt)
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showstashstate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+    set -g __fish_git_prompt_showcolorhints 1
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_showupstream verbose git
+    set -g __fish_git_prompt_char_stateseparator ""
 
     set_color normal
+    echo (__fish_git_prompt ' %s')
 end

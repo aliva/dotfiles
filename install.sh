faf2ef47643eda482227a08e2e475b5c0ced990b
Ali Vakilzade <ali@vakilzade.com>
2019-05-07 12:48:00 +0430

    Update .gitignore, install.sh, nvim/init.vim, vscode/settings.json

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 
 export DOTFILES=$HOME/Workspace/dotfiles
 
-curl -fLo $DOTFILES/nvim/autoload/vim.plug --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $DOTFILES/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim

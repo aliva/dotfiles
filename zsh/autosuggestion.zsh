8578af7c1eb04d4cc9139341f47cafe194da7e9b
Ali Vakilzade <ali@digitgaming.com>
2019-11-25 12:25:40 +0000

    moar

--- zsh/autosuggestion.zsh
+++ zsh/autosuggestion.zsh
@@ -4,6 +4,8 @@ if [ -f /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh ]; th
   source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
 elif [ -f /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
   source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+elif [ -f /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
+  source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 fi
 
 export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=7'

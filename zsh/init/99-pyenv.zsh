c003b88588bbc03ff0a6ca8da6cc0e005cc2b4cc
Ali Vakilzade <ali@vakilzade.com>
2022-10-06 15:37:05 +0100

    verify pyenv

--- zsh/init/99-pyenv.zsh
+++ zsh/init/99-pyenv.zsh
@@ -1,6 +1,7 @@
-if [ -x "$(command -v pyenv)" ]
+export PYENV_ROOT="$HOME/.pyenv"
+
+if [ -x "$(command -v pyenv)" ] || [ -d $PYENV_ROOT ]
 then
-    export PYENV_ROOT="$HOME/.pyenv"
     export PATH="$PYENV_ROOT/bin:$PATH"
     eval "$(pyenv init --path)"
 fi

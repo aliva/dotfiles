d653b68f75d0f81b75570d247bb6df85dd5e7bd3
Ali Vakilzade <ali@vakilzade.com>
2022-12-13 13:14:24 +0000

    more i3 configs

--- i3/xinit.sh
+++ i3/xinit.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-setxkbmap -layout us,ir
-xrandr --output DP-1-2 --mode '3840x2160' --rate 59.97 --left-of eDP-1
+set -x
+
+setxkbmap -layout us, -option "grp:win_space_toggle"
+xrdb ~/.config/i3/Xresources
+
+# Display
+LID_STATE=`cat /proc/acpi/button/lid/LID/state  | cut -d ':' -f 2 | xargs`
+LAPTOP_LCD="eDP-1"
+EXTERNAL_LCD="DP-1-2"
+
+if [ $LID_STATE = "closed" ]
+then
+    xrandr \
+        --output $EXTERNAL_LCD --mode '3840x2160' --rate 59.97 \
+        --output $LAPTOP_LCD --off
+
+else
+    xrandr \
+        --output $EXTERNAL_LCD --mode '3840x2160' --rate 59.97 \
+        --left-of $LAPTOP_LCD
+fi
+

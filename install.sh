fa8ef622edd08699b7bd88a38dec1257dfdcf17e
Ali Vakilzade <ali@vakilzade.com>
2023-02-01 18:19:04 +0000

    vim plug updated

--- install.sh
+++ install.sh
@@ -34,9 +34,3 @@ fi
 
 echo "syntax:glob" > $DOTFILES/hgignore
 cat $DOTFILES/gitignore >> $DOTFILES/hgignore
-
-if [[ ! -f $DOTFILES/nvim/autoload/plug.vim  ]]; then
-  curl -fLo $DOTFILES/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
-

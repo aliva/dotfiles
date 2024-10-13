a7876309a626a2730a4ce55f81c82043b0185722
Ali Vakilzade <ali@vakilzade.com>
2024-10-13 22:49:55 +0100

    config rofi

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ export DOTFILES=$HOME/Workspace/dotfiles
 
 rm -rf $HOME/.config/Code/User
 rm -rf $HOME/.config/nvim
+rm -rf $HOME/.config/rofi
 rm -rf $HOME/.config/sway
 rm -rf $HOME/.config/waybar
 
@@ -13,25 +14,7 @@ mkdir -p $HOME/.aria2/
 mkdir -p $HOME/.config/
 mkdir -p $HOME/.config/Code/
 mkdir -p $HOME/.config/Code\ -\ OSS/
-
-ln -sf $DOTFILES/ackrc          $HOME/.ackrc
-ln -sf $DOTFILES/aria2.conf     $HOME/.aria2/aria2.conf
-ln -sf $DOTFILES/bashrc         $HOME/.bashrc
-ln -sf $DOTFILES/editorconfig   $HOME/.editorconfig
-ln -sf $DOTFILES/flake8         $HOME/.config/flake8
-ln -sf $DOTFILES/gitconfig      $HOME/.gitconfig
-ln -sf $DOTFILES/isort.cfg      $HOME/.isort.cfg
-ln -sf $DOTFILES/npmrc          $HOME/.npmrc
-ln -sf $DOTFILES/nvim           $HOME/.config/nvim
-ln -sf $DOTFILES/profile        $HOME/.profile
-ln -sf $DOTFILES/starship.toml  $HOME/.config/starship.toml
-ln -sf $DOTFILES/sway           $HOME/.config/sway
-ln -sf $DOTFILES/tool-versions  $HOME/.tool-versions
-ln -sf $DOTFILES/tmux.conf      $HOME/.tmux.conf
-ln -sf $DOTFILES/vscode         $HOME/.config/Code/User
-ln -sf $DOTFILES/vscode         $HOME/.config/Code\ -\ OSS/User
-ln -sf $DOTFILES/waybar         $HOME/.config/waybar
-ln -sf $DOTFILES/zshrc          $HOME/.zshrc
+mkdir -p $HOME/.config/rofi
 
 # MacOS
 if [[ `uname -s` == "Darwin" ]]; then
@@ -57,5 +40,32 @@ fi
 if ! type starship > /dev/null && [[ ! -f $HOME/.local/bin/starship  ]]; then
   curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir $HOME/.local/bin --yes
 fi
+
+# dracula
+if [[ ! -d $DOTFILES/external/dracula-rofi ]]; then
+  git clone https://github.com/dracula/rofi $DOTFILES/external/dracula-rofi
+fi
+
+ln -sf $DOTFILES/ackrc          $HOME/.ackrc
+ln -sf $DOTFILES/aria2.conf     $HOME/.aria2/aria2.conf
+ln -sf $DOTFILES/bashrc         $HOME/.bashrc
+ln -sf $DOTFILES/editorconfig   $HOME/.editorconfig
+ln -sf $DOTFILES/flake8         $HOME/.config/flake8
+ln -sf $DOTFILES/gitconfig      $HOME/.gitconfig
+ln -sf $DOTFILES/isort.cfg      $HOME/.isort.cfg
+ln -sf $DOTFILES/npmrc          $HOME/.npmrc
+ln -sf $DOTFILES/nvim           $HOME/.config/nvim
+ln -sf $DOTFILES/profile        $HOME/.profile
+ln -sf $DOTFILES/starship.toml  $HOME/.config/starship.toml
+ln -sf $DOTFILES/sway           $HOME/.config/sway
+ln -sf $DOTFILES/tmux.conf      $HOME/.tmux.conf
+ln -sf $DOTFILES/tool-versions  $HOME/.tool-versions
+ln -sf $DOTFILES/vscode         $HOME/.config/Code/User
+ln -sf $DOTFILES/vscode         $HOME/.config/Code\ -\ OSS/User
+ln -sf $DOTFILES/waybar         $HOME/.config/waybar
+ln -sf $DOTFILES/zshrc          $HOME/.zshrc
+
+# External
+ln -sf $DOTFILES/external/dracula-rofi/theme/config1.rasi $HOME/.config/rofi/config.rasi
 
 echo "Done."

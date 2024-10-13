dbbc48e545c3df96f9d8478eb4c53cec5cbd0fde
Ali Vakilzade <ali@vakilzade.com>
2024-10-14 00:17:12 +0100

    install swaylock

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ rm -rf $HOME/.config/Code/User
 rm -rf $HOME/.config/nvim
 rm -rf $HOME/.config/rofi
 rm -rf $HOME/.config/sway
+rm -rf $HOME/.config/swaylock
 rm -rf $HOME/.config/waybar
 
 mkdir -p $HOME/.aria2/
@@ -15,6 +16,7 @@ mkdir -p $HOME/.config/
 mkdir -p $HOME/.config/Code/
 mkdir -p $HOME/.config/Code\ -\ OSS/
 mkdir -p $HOME/.config/rofi
+mkdir -p $HOME/.config/swaylock
 
 # MacOS
 if [[ `uname -s` == "Darwin" ]]; then
@@ -45,6 +47,9 @@ fi
 if [[ ! -d $DOTFILES/external/dracula-rofi ]]; then
   git clone https://github.com/dracula/rofi $DOTFILES/external/dracula-rofi
 fi
+if [[ ! -d $DOTFILES/external/dracula-swaylock ]]; then
+  git clone https://github.com/dracula/swaylock $DOTFILES/external/dracula-swaylock
+fi
 
 ln -sf $DOTFILES/ackrc          $HOME/.ackrc
 ln -sf $DOTFILES/aria2.conf     $HOME/.aria2/aria2.conf
@@ -67,5 +72,7 @@ ln -sf $DOTFILES/zshrc          $HOME/.zshrc
 
 # External
 ln -sf $DOTFILES/external/dracula-rofi/theme/config1.rasi $HOME/.config/rofi/config.rasi
+ln -sf $DOTFILES/external/dracula-swaylock/swaylock/config $HOME/.config/swaylock/config
+
 
 echo "Done."

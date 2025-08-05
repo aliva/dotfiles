58462ba2b938bb90ea5f9eafe1f484023de907a8
Ali Vakilzade <ali@vakilzade.com>
2025-08-05 23:42:44 +0100

    cleanup

---
+++ arch.sh
@@ -0,0 +1,4 @@
+
+sudo pacman -Suy \
+    git \
+    zsh
--- install.sh
+++ install.sh
@@ -6,16 +6,10 @@ export DOTFILES=$HOME/Workspace/dotfiles
 
 rm -rf $HOME/.config/Code/User
 rm -rf $HOME/.config/nvim
-rm -rf $HOME/.config/rofi
-rm -rf $HOME/.config/sway
-rm -rf $HOME/.config/swaylock
-rm -rf $HOME/.config/waybar
 
 mkdir -p $HOME/.aria2/
 mkdir -p $HOME/.config/
 mkdir -p $HOME/.config/Code/
-mkdir -p $HOME/.config/Code\ -\ OSS/
-mkdir -p $HOME/.config/rofi
 
 # MacOS
 if [[ `uname -s` == "Darwin" ]]; then
@@ -37,17 +31,6 @@ if [[ ! -f $DOTFILES/nvim/autoload/plug.vim  ]]; then
   curl -fLo $DOTFILES/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
-# starship
-if ! type starship > /dev/null && [[ ! -f $HOME/.local/bin/starship  ]]; then
-  curl -sS https://starship.rs/install.sh | sh -s -- --bin-dir $HOME/.local/bin --yes
-fi
-
-# dracula
-if [[ ! -d $DOTFILES/external/dracula-rofi ]]; then
-  git clone https://github.com/dracula/rofi $DOTFILES/external/dracula-rofi
-fi
-
-ln -sf $DOTFILES/ackrc          $HOME/.ackrc
 ln -sf $DOTFILES/aria2.conf     $HOME/.aria2/aria2.conf
 ln -sf $DOTFILES/bashrc         $HOME/.bashrc
 ln -sf $DOTFILES/editorconfig   $HOME/.editorconfig
@@ -58,17 +41,9 @@ ln -sf $DOTFILES/npmrc          $HOME/.npmrc
 ln -sf $DOTFILES/nvim           $HOME/.config/nvim
 ln -sf $DOTFILES/profile        $HOME/.profile
 ln -sf $DOTFILES/starship.toml  $HOME/.config/starship.toml
-ln -sf $DOTFILES/sway           $HOME/.config/sway
-ln -sf $DOTFILES/swaylock       $HOME/.config/swaylock
 ln -sf $DOTFILES/tmux.conf      $HOME/.tmux.conf
 ln -sf $DOTFILES/tool-versions  $HOME/.tool-versions
 ln -sf $DOTFILES/vscode         $HOME/.config/Code/User
-ln -sf $DOTFILES/vscode         $HOME/.config/Code\ -\ OSS/User
-ln -sf $DOTFILES/waybar         $HOME/.config/waybar
 ln -sf $DOTFILES/zshrc          $HOME/.zshrc
-
-# External
-ln -sf $DOTFILES/external/dracula-rofi/theme/config1.rasi $HOME/.config/rofi/config.rasi
-
 
 echo "Done."
